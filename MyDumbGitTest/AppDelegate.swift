10ce328d82967282c8fb17bc12c3b52903a5ae40
Dioretix <bbraksick@mac.com>
2018-09-24 13:45:37 -0400

    My dumb

--- MyDumbGitTest/AppDelegate.swift
+++ MyDumbGitTest/AppDelegate.swift
@@ -16,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        debugPrint("some dumb launch message")
         return true
     }
 
